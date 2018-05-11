25088fd5a4006be92ddacb50634be11f2d9f8403
dougeff <dougfraker@gmail.com>
2018-05-10 22:21:53 -0400

    Update Valid.py

--- Valid.py
+++ Valid.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # NES filesize checker, gutted from 6502d
-# Doug Fraker 2017
+# Doug Fraker 2017-2018
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
 # this software and associated documentation files (the "Software"), to deal in the 
